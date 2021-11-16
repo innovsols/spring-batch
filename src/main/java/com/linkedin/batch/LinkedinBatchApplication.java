f660c8372c770811ee820d0b17b53f0daa768f7a
Gorakhnath Ghadage <innovating.solution@gmail.com>
2021-11-16 17:13:09 +0530

    commit with mysql usage

--- src/main/java/com/linkedin/batch/LinkedinBatchApplication.java
+++ src/main/java/com/linkedin/batch/LinkedinBatchApplication.java
@@ -38,7 +38,9 @@ public class LinkedinBatchApplication {
 			
 			@Override
 			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-				System.out.println("The item has been packaged");
+				String item = chunkContext.getStepContext().getJobParameters().get("item").toString();
+				String date = chunkContext.getStepContext().getJobParameters().get("run.date").toString();
+				System.out.println(String.format("The item %s has been packaged on %s",item,date));
 				return RepeatStatus.FINISHED;
 			}
 		}).build();
