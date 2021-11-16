3cc429657afadc7e4282b9f94f360122f41a6bd9
Gorakhnath Ghadage <innovating.solution@gmail.com>
2021-11-16 12:58:39 +0530

    initial commit of LinkedinApplication

--- src/main/java/com/linkedin/batch/LinkedinBatchApplication.java
+++ src/main/java/com/linkedin/batch/LinkedinBatchApplication.java
@@ -1,12 +1,48 @@
 package com.linkedin.batch;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties.Build;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableBatchProcessing
 public class LinkedinBatchApplication {
+	
+	@Autowired
+	public JobBuilderFactory jobBuilderFactory;
+	
+	@Autowired
+	public StepBuilderFactory sBuilderFactory;
+	
+	@Bean
+	public Job deliverPackageJob() {
+		return this.jobBuilderFactory.get("deliverPackageJob").start(packageItemStep())
+					.build();
+	}
+
+	@Bean
+	public Step packageItemStep() {
+		// TODO Auto-generated method stub
+		return this.sBuilderFactory.get("packageItemStep").tasklet(new Tasklet() {
+			
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				System.out.println("The item has been packaged");
+				return RepeatStatus.FINISHED;
+			}
+		}).build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LinkedinBatchApplication.class, args);
